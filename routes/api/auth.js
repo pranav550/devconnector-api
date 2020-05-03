fac49310aa4e7736102364d70f81061aa471cb5d
pranav550 <pranavverma550>
2020-05-03 12:55:21 +0530

    register , login

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,71 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
-// @route GET api/auths
+// @route POST api/auth
 // desc   Test route
 // access public
 
-router.get("/", (req, res) => {
-  res.send("auths route");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
 });
+
+router.post(
+  "/",
+  [
+    check("email", "Valid Email Is required").isEmail(),
+    check("password", "Please is Required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      // if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ errors: "Invalid Credentials" });
+      }
+
+      // password compare
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ errors: "Invalid Credentials" });
+      }
+      // return jwt token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).json("server error");
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/user.js
+++ routes/api/user.js
@@ -63,7 +63,7 @@ router.post(
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
-          res.json({ token });
+          res.json({ user, token });
         }
       );
     } catch (err) {
